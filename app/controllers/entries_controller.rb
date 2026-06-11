4c87e88138de916fa0c1997f847a450b01833df8
Peerawat.Chupipat <Peerawat.chupipat@kellogg.northwestern.edu>
2026-06-11 22:03:34 +0000

    Filter entries by owner and hide new entry link from logged-out users

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,16 +1,24 @@
 class EntriesController < ApplicationController
 
   def new
+    if @current_user == nil
+      redirect_to "/login"
+    end
   end
 
   def create
-    @entry = Entry.new
-    @entry["title"] = params["title"]
-    @entry["description"] = params["description"]
-    @entry["occurred_on"] = params["occurred_on"]
-    @entry["place_id"] = params["place_id"]
-    @entry.save
-    redirect_to "/places/#{@entry["place_id"]}"
+    if @current_user != nil
+      @entry = Entry.new
+      @entry["title"] = params["title"]
+      @entry["description"] = params["description"]
+      @entry["occurred_on"] = params["occurred_on"]
+      @entry["place_id"] = params["place_id"]
+      @entry["user_id"] = @current_user["id"]
+      @entry.save
+      redirect_to "/places/#{@entry["place_id"]}"
+    else
+      redirect_to "/login"
+    end
   end
 
 end
